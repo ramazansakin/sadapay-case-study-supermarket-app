8626c4e500a3d4d82e921a35b70cbdad51727f5f
ramazansakin <ramazansakin63@gmail.com>
2022-11-21 14:52:26 +0300

    chore: RED > I tried to see the failure when I try to doCommand first instead of checkout so I need to handle this edge case

--- solution/src/test/java/com/rsakin/sadapay/casestudy/SuperMarketTest.java
+++ solution/src/test/java/com/rsakin/sadapay/casestudy/SuperMarketTest.java
@@ -37,10 +37,19 @@ class SuperMarketTest {
     }
 
     @Test
-    void shouldRunCommandCheckout(){
+    void shouldRunCommandCheckout() {
         SuperMarket superMarket = new SuperMarket(".\\..\\functional_spec\\fixtures\\inventory.csv");
         superMarket.doCommand("checkout");
         assertEquals("empty cart\r\n", outContent.toString());
+    }
+
+    // First we need to checkout to start shopping
+    @Test
+    void shouldNotAddItemToCart_NeedToCheckoutFirst() {
+        // we need to create a loop to get commands and try to get checkout first to start shopping
+        SuperMarket superMarket = new SuperMarket(".\\..\\functional_spec\\fixtures\\inventory.csv");
+        superMarket.doCommand("add soap 5");
+        assertEquals("You need to 'checkout' first to start shopping!\r\n", outContent.toString());
     }
 
 }
