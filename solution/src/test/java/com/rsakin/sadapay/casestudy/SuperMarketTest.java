9b5d785763031df21ecbc60ccd282cf0ac50ac34
ramazansakin <ramazansakin63@gmail.com>
2022-11-21 14:03:36 +0300

    feat: first RED cycle > there is an inventory file and we need to read all the lines properly to build inventory , there is no supermarket class and buildInventory function so we ll get error and then we ll start working on it to create related classes and methods to initialize functionality of supermarket

---
+++ solution/src/test/java/com/rsakin/sadapay/casestudy/SuperMarketTest.java
@@ -0,0 +1,15 @@
+package com.rsakin.sadapay.casestudy;
+
+import org.junit.jupiter.api.Test;
+
+class SuperMarketTest {
+
+    // First we have inventory file and we need to read all the lines properly
+    @Test
+    void shouldGetInventoryFileAndPrintContent() {
+        // supermarket init with inventory file
+        SuperMarket superMarket = new SuperMarket(".\\..\\functional_spec\\fixtures\\inventory.csv");
+        superMarket.buildInventory();
+    }
+
+}
