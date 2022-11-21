f6276cb852e4a439b96cc1704fe7b18b275ba388
ramazansakin <ramazansakin63@gmail.com>
2022-11-21 14:33:44 +0300

    feat: GREEN > Exception handled and stderr message covered with assertion to validate error message

--- solution/src/main/java/com/rsakin/sadapay/casestudy/Inventory.java
+++ solution/src/main/java/com/rsakin/sadapay/casestudy/Inventory.java
@@ -12,10 +12,14 @@ public class Inventory {
 
     private void buildInventory(String inventoryFile) throws IOException {
         // To build the inventory, we need to parse all the lines to fill items and add to inventory item list
-        final var allLines =
-                Files.readAllLines(Paths.get(".", inventoryFile));
+        try {
+            final var allLines =
+                    Files.readAllLines(Paths.get(".", inventoryFile));
 
-        allLines.forEach(System.out::println);
+            allLines.forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.println("There is no such file [ " + inventoryFile + " ]");
+        }
 
     }
 
--- solution/src/test/java/com/rsakin/sadapay/casestudy/InventoryTest.java
+++ solution/src/test/java/com/rsakin/sadapay/casestudy/InventoryTest.java
@@ -1,16 +1,39 @@
 package com.rsakin.sadapay.casestudy;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static junit.framework.TestCase.assertEquals;
 
 class InventoryTest {
+
+    // To catch the stdout and stderr messages and assert with expected ones
+    protected final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    protected final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    protected final PrintStream originalOut = System.out;
+    protected final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 
     @Test
     void shouldGetErrorWhenNoSuchFileState() throws IOException {
         Inventory inventory = new Inventory("xxx.csv");
+        assertEquals("There is no such file [ xxx.csv ]\r\n", errContent.toString());
     }
 
 }
