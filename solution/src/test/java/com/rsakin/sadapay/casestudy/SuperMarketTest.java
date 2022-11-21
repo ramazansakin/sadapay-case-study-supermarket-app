8e84ab74af2f54c8ca20eb5eebb129be174bb9c8
ramazansakin <ramazansakin63@gmail.com>
2022-11-21 14:41:12 +0300

    chore: RED > when I try to 'chekout' on supermarket, I could get an empty cart to start shopping, now it s failing

--- solution/src/test/java/com/rsakin/sadapay/casestudy/SuperMarketTest.java
+++ solution/src/test/java/com/rsakin/sadapay/casestudy/SuperMarketTest.java
@@ -1,8 +1,32 @@
 package com.rsakin.sadapay.casestudy;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
+
 class SuperMarketTest {
+    // To catch the stdout and stderr messages and assert with expected ones
+    protected final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    protected final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    protected final PrintStream originalOut = System.out;
+    protected final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 
     // First we have inventory file and we need to read all the lines properly
     @Test
@@ -10,6 +34,13 @@ class SuperMarketTest {
         // supermarket init with inventory file
         SuperMarket superMarket = new SuperMarket(".\\..\\functional_spec\\fixtures\\inventory.csv");
 
+    }
+
+    @Test
+    void shouldRunCommandCheckout(){
+        SuperMarket superMarket = new SuperMarket(".\\..\\functional_spec\\fixtures\\inventory.csv");
+        superMarket.doCommand("checkout");
+        assertEquals("empty chart\r\n", outContent.toString());
     }
 
 }
