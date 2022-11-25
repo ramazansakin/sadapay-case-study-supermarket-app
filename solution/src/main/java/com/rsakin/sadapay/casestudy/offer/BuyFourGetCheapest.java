517380decc38edf998fb570c6e155d9ea58c8540
ramazansakin <ramazansakin63@gmail.com>
2022-11-25 14:04:39 +0300

    REFACTOR > need to add a condition to make a valid offer

--- solution/src/main/java/com/rsakin/sadapay/casestudy/offer/BuyFourGetCheapest.java
+++ solution/src/main/java/com/rsakin/sadapay/casestudy/offer/BuyFourGetCheapest.java
@@ -8,6 +8,9 @@ public class BuyFourGetCheapest implements OfferableCart {
 
     @Override
     public double calculateDiscount(Set<Item> itemOfferedList) {
+        if (itemOfferedList.size() != 4) {
+            System.err.println("The offer can not be applied to cart. You need to buy just 4 items");
+        }
         Item item = itemOfferedList.stream().sorted().findFirst().get();
         double amount = itemOfferedList.size() * item.getPrice();
         double subTotal = itemOfferedList.stream().mapToDouble(Item::getPrice).reduce(0, Double::sum);
